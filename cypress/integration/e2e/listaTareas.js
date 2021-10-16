f027fb5b576ccc02b53e965942f2b8703c16903c
Andres Suarez <andres.suarez@mediamonks.com>
2021-10-16 17:42:18 -0300

    feat: creado test de id autoincremental

--- cypress/integration/e2e/listaTareas.js
+++ cypress/integration/e2e/listaTareas.js
@@ -16,12 +16,31 @@ describe('Pantalla de Lista de Tareas', () => {
 
       cy.contains('[data-testid="tableRowId"]', 1)
       cy.contains('[data-testid="tableRowDesc"]', 'Nueva tarea')
-    });
-    
+    })
+
     it('Si no se ingresa una descripcion, se muestra un mensaje de error', () => {
       getBotonAgregarTarea().click()
 
       getByDataTestId('errorMessage')
     })
+
+    describe('Cuando se agregan dos tareas correctamente', () => {
+      beforeEach(() => {
+        // Crear tarea 1
+        getInputDescripcionDeTarea().type('Tarea 1')
+        getBotonAgregarTarea().click()
+
+        //Crear tarea 2
+        getInputDescripcionDeTarea().type('Tarea 2')
+        getBotonAgregarTarea().click()
+      });
+
+      it('El ID aumenta de forma incremental', () => {
+        getByDataTestId('tableRowId').each((item, index) => {
+          cy.wrap(item).should('contain.text', (index + 1).toString())
+        })
+      })
+    })
+
   })
 })
